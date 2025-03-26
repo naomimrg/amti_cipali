5423b719cf9f29b5803fa68113d539d0fd6f893f
syarif <syariffortask@gmail.com>
2025-03-26 23:01:06 +0700

    fix status

--- resources/views/vendor/lokasi.blade.php
+++ resources/views/vendor/lokasi.blade.php
@@ -256,8 +256,9 @@
             const element = document.getElementById(elementId);
 
             if (sensor) {
-                element.innerText = `${sensor.max_value.toFixed(1)} ${sensorNamePart === 'Full_Bridge' ? 'Microstrain' : 'mm'}`;
-                drawGauge(canvasId, parseInt(sensor.max_value), parseInt(sensor.batas_atas), parseInt(sensor.batas_bawah));
+                const value = sensor.max_value.toFixed(1); 
+                element.innerText = `${value} ${sensorNamePart === 'Full_Bridge' ? 'Microstrain' : 'mm'}`;
+                drawGauge(canvasId, parseInt(value), parseInt(sensor.batas_atas), parseInt(sensor.batas_bawah));
             } else {
                 element.innerText = "No data";
             }
