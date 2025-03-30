ec66aface913664909b8831fa864efb46d6e399d
syarif <syariffortask@gmail.com>
2025-03-30 16:11:40 +0700

    add class card-body-canvas di admin vendor

--- resources/views/admin_vendor/dashboard/index.blade.php
+++ resources/views/admin_vendor/dashboard/index.blade.php
@@ -87,7 +87,7 @@
             </div> 
             <div class="col-12">
                 <div class="card">
-                    <div class="card-body"> 
+                    <div class="card-body card-body-canvas"> 
                         <canvas id="myCanvas" class="w-100"></canvas>
                     </div>
                 </div>
